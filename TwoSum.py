8b9e0fb6852ca4fc8c69366e5201d36fac10bbd2
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-09-05 22:42:02 +0300

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,29 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num1 = 0
+        num2 = 0 
+        i = 0
+        
+        for i in range(0, len(nums)+1):
+            num1 = nums[i]
+            k = 0
+            j = 0
+            last_element = len(nums) -1
+            while j != (last_element +1):
+                if j != i:
+                    num2 = nums[j]
+                    k = num1 + num2
+                if k == target:
+                    break
+                j += 1
+            if k == target:
+                break
+        
+        list_indx = [i, j]
+        
+        return list_indx
